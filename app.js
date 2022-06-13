1e4f5aed3036cc93bc2591e8da957263fa670677
Dvhj <kirillov97@list.ru>
2022-06-14 00:43:31 +0400

    adaptive

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const menu = document.querySelectorAll('.menu_choice')
+const menuChoice = document.querySelectorAll('.menu_choice')
+const menu = document.querySelector('.menu')
+const menuBox = document.querySelector('.menu_box')
 const pause_block = document.querySelector('.pause_block')
 const real_age = document.querySelector('.real_age')
 const choice = document.querySelectorAll('.choice')
@@ -6,7 +8,22 @@ const blocksQuestions = document.querySelectorAll('.real_age__boxes')
 const conslusion = document.querySelector('.conslusion_btn')
 const result = document.querySelector('.result')
 
-menu.forEach(item => {
+let menu_count = false
+
+menu.addEventListener('click', () => {
+	if (window.screen.width <= 480) {
+		if (menu_count == false) {
+			menuBox.style.display = 'block'
+			menu_count = true
+		} else {
+			menuBox.style.display = 'none'
+			menu_count = false
+		}
+		
+	}
+})
+
+menuChoice.forEach(item => {
 	item.addEventListener('click', () => {
 			if (item.dataset.menu == 'age') {
 				pause_block.style.display = 'none'
